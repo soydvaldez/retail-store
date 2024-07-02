325c4027c26415a1c5b08176758320d3bd94246f
dvaldez <dvaldez@no-reply.com>
2024-07-02 14:51:09 -0600

    TA #0016 Deploy Config - Scripts -Se agrego una funcion: "logs" para soportar el registro de actividad mediante la captura de logs del script:  Util para hacer tracking de los steps que se estan ejecutando y llevar acabo un registro detallado del estado de los procesos.
    -Se agrego una funcion: "goToSleep" para agregar un timeout de un segundo, en la ejecuccion de cada funcion
    
    -Se agrego una funcion: "getCurrentTime" para manejo de la fecha y hora la cual se agrega a cada registro de actividad registrada en los logs

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#Utilerias para generar y gestionar la imagen de la aplicacion: retail-store.
-#supuesto: El registry esta levantado, la imagen se creo con exito
+#Utilerias para generar y gestionar la imagen de la aplicacion: retail-store. dependencias: El registry esta levantado, la imagen se creo con exito
 
-set -e
+SCRIPT=$0
+echo "\nInicializando script: $SCRIPT"
 
 IMG_NAME="localhost:5000/retail-store:1.0.0"
 CONTAINER_NAME="retail-store"
 
 PATH_LOGS_FILE=scripts/logs/utilities_activity.log
 
-LOGFILE=scripts/logs/utilities_activity1.log
-BUILD_LOG=scripts/logs/utilities_activity2.log
+LOGFILE=./scripts/logs/utilities_activity1.log
+BUILD_LOG=./scripts/logs/utilities_activity2.log
+
+echo "Variables setteadas dentro del contexto de script: IMG_NAME:$IMG_NAME \tCONTAINER_NAME: $CONTAINER_NAME"
+echo "LOGFILE:$LOGFILE \tBUILD_LOG:$BUILD_LOG"
 
 error_handler() {
     local lineno=$1
@@ -18,7 +21,7 @@ error_handler() {
     echo "Error en la línea $lineno dentro de la función $funcname"
 }
 
-trap 'error_handler $LINENO' ERR
+# trap 'error_handler $LINENO' ERR
 
 check_error() {
     if [[ "$?" -ne 0 ]]; then
@@ -26,35 +29,48 @@ check_error() {
     fi
 }
 
-#Verifica el estado de un contenedor
-state_container() {
-    CONTAINER_TO_VERIFY="$1"
-    echo "Verificando la salud del contenedor: " "\t[$CONTAINER_TO_VERIFY]"
-    salida=$(
-        CONTAINER_NAME="registry"
-        docker ps -a --filter "name=$CONTAINER_NAME" | awk 'NR>1 {print $1, $NF}'
-    )
-
-    if [[ "$salida" = "" ]]; then
-        echo "la cadena no esta vacia, continuando"
-        echo "segundo mensaje"
-    else echo "sino se cumple haz esto"; fi
-    # docker container inspect ${CONTAINER} | jq '.[].State'
+timeToSleep=1
+goToSleep() {
+    sleep $timeToSleep
 }
 
-#Construye la imagen del proyecto:  Agrega un tag para nombrar y poder identificar la url del servidor docker registry.
-create() {
+currentTime=""
+getCurrentTime() {
+    currentTime="[$(date +"%Y-%m-%d %H:%M:%S")]"
+}
+
+# Imprime por terminal y captura los logs de script
+logs() {
+    getCurrentTime
+    if [[ $2 -eq 1 ]]; then
+        echo "\n$currentTime $1" | tee -a "./scripts/temporal.log"
+    else
+        echo "$currentTime $1" | tee -a "./scripts/temporal.log"
+    fi
+}
+
+#Construye la imagen del proyecto:  Agrega un tag para nombrar y poder identificar la url destino para guardarla en el servidor: docker registry.
+createImage() {
+    goToSleep
+    logs "Step1: Create Docker Image \t[start]" "1"
+
     docker build -t ${IMG_NAME} . 2> >(tee -a $LOGFILE >&2) | tee -a $BUILD_LOG
     check_error "Error en la construccion de la imagen Docker"
+
+    logs "Step1: Create Docker Image \t[done]"
 }
 
-#Agrega la imagen del proeycto al servidor de docker registry
-addToRegistry() {
-    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Funcion actual: add_to_registry"
+#Agrega la imagen del proyecto al servidor de docker registry
+addImageToRegistry() {
+    goToSleep
+    logs "Step2: Add Image To Docker Registry" "1" "\t[start]"
+    logs "Docker Registry Running at URL: localhost:5000 \t[running]"
+
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] Currently Running Function: add_to_registry"
     local CONTAINER_NAME="registry"
 
     is_container_exist() {
-        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Funcion anidada (actual): is_container_exist  \t[OK]"
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Currently Running Function: is_container_exist  \t[OK]"
         isContainerExists=$(docker ps -a --format "{{.Names}}" | awk "/${CONTAINER_NAME}/")
         if [ "${isContainerExists}" = "" ]; then
             echo "No existe el contenedor: $(echo $CONTAINER_NAME | tr 'a-z' 'A-Z')" | tee "$PATH_LOGS_FILE"
@@ -70,7 +86,7 @@ addToRegistry() {
 
     # Aqui tengo una dependencia:
     is_container_running() {
-        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Funcion anidada (actual)] is_container_running \t[OK]"
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Currently Running Function: is_container_running \t[OK]"
         state_container=$(docker container inspect registry | jq '.[].State')
         if [[ $(echo $state_container | jq '.Running') == "false" ]]; then
             echo "No esta corriendo el contenedor"
@@ -81,7 +97,7 @@ addToRegistry() {
     }
 
     check_health_container() {
-        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Funcion anidada (actual)] check_health_container \t[OK]"
+        echo "[$(date +"%Y-%m-%d %H:%M:%S")] Currently Running Function ] check_health_container \t[OK]"
         URL="localhost:5000/v2/_catalog"
         http_code=$(curl -o /dev/null -s -w "%{http_code}" "$URL" | tr -d "\r")
         if [ "$http_code" -eq 200 ]; then
@@ -89,19 +105,31 @@ addToRegistry() {
         fi
     }
 
-    #Dependencias
+    #Dependencias:
     is_container_exist && is_container_running && check_health_container && docker push ${IMG_NAME}
+    logs "Step2: Add Image To Docker Registry \t[done]"
 }
 
-running_container() {
+deployAndStartContainer() {
+    goToSleep
+    logs "Step3: Deploy And Start Container \t[start]" "1"
     docker run --name retail-store -p 8080:8080 ${IMG_NAME}
+    logs "Step3: Deploy And Start Container \t[done]"
 }
 
 run() {
-    create && addToRegistry
-    # running_container
+    createImage
+    addImageToRegistry
+    deployAndStartContainer
+    goToSleep
+    logs "Finalizo la ejecuccion del script: $SCRIPT" "1"
 }
 
-# run
-URL="localhost:8080/api/products"
-curl -s -u admin:password  $URL | jq ".content.[0]"
+run
+
+# URL="localhost:8080/api/products"
+# curl -s -u admin:password  $URL | jq ".content.[0]"
+
+# Levantar el servidor consumir algunas rutas
+
+# Mejorar: Debo de agregar un trace para registrar en que step fallo y poder volver a ejecutar desde ese punto en especifico
